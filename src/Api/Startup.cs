c85defe0a6500d0b9599900f464755b758176827
Jorge Candeias <candeias@fastmail.fm>
2018-11-20 22:54:05 +0000

    Added basic client startup code to api.

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using System;
+using System.Threading.Tasks;
 
 namespace Api
 {
@@ -19,10 +24,21 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // configure the orleans client
+            services.AddSingleton(new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "MyCluster";
+                    options.ServiceId = "MyService";
+                })
+                .ConfigureLogging(config => config.AddDebug())
+                .Build());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger, IClusterClient client)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +51,18 @@ namespace Api
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // connect to the orleans cluster
+            logger.LogInformation("Connecting to the Orleans cluster...");
+            var attempt = 1;
+            client.Connect(async error =>
+            {
+                logger.LogWarning("Failed to connect to the Orleans cluster on attempt {@Attempt}.", attempt);
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                ++attempt;
+                return true;
+            }).Wait();
+            logger.LogInformation("Connected to the Orleans cluster after {@Attempts} attempt(s).", attempt);
         }
     }
 }
