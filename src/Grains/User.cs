66d09ebc32abdbf3d0056186bd44a176adb3fa38
Jorge Candeias <candeias@fastmail.fm>
2018-11-21 13:53:37 +0000

    Added Ping method to User grain.

--- src/Grains/User.cs
+++ src/Grains/User.cs
@@ -1,9 +1,14 @@
-﻿using Interfaces;
+﻿using System.Threading.Tasks;
+using Interfaces;
 using Orleans;
 
 namespace Grains
 {
     public class User : Grain, IUser
     {
+        public Task Ping()
+        {
+            return Task.CompletedTask;
+        }
     }
 }
--- src/Interfaces/IUser.cs
+++ src/Interfaces/IUser.cs
@@ -1,8 +1,10 @@
 ﻿using Orleans;
+using System.Threading.Tasks;
 
 namespace Interfaces
 {
     public interface IUser: IGrainWithGuidKey
     {
+        Task Ping();
     }
 }
