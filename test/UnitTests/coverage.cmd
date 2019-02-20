d132ce18561b501ffd990f7117a5a1960eaabbdb
Jorge Candeias <candeias@fastmail.fm>
2019-02-20 00:20:59 +0000

    Added OpenCover.

---
+++ test/UnitTests/coverage.cmd
@@ -0,0 +1,7 @@
+"%HOMEDRIVE%%HOMEPATH%\.nuget\packages\opencover\4.7.922\tools\OpenCover.Console.exe"^
+ -target:"%ProgramFiles%\dotnet\dotnet.exe"^
+ -targetargs:"test"^
+ -output:coverage.xml^
+ -oldStyle^
+ -register:user^
+ -filter:"+[*]* -[UnitTests]*"
