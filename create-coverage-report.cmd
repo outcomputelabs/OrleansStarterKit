56e953b01fa8335226e82b720d2d9d685c116125
Jorge Candeias <candeias@fastmail.fm>
2019-02-23 22:29:36 +0000

    Added coveralls integration.

---
+++ create-coverage-report.cmd
@@ -0,0 +1,10 @@
+"%HOMEDRIVE%%HOMEPATH%\.nuget\packages\opencover\4.7.922\tools\OpenCover.Console.exe"^
+ -target:"%ProgramFiles%\dotnet\dotnet.exe"^
+ -targetargs:"test"^
+ -output:coverage.xml^
+ -oldStyle^
+ -register:user^
+ -filter:"+[Client]* +[Grains]* +[Grains.Abstractions]* +[Silo]* -[UnitTests]* -[Database]*"^
+ -excludebyattribute:"*.GeneratedCode*"
+
+dotnet "%HOMEDRIVE%%HOMEPATH%\.nuget\packages\reportgenerator\4.0.12\tools\netcoreapp2.0\ReportGenerator.dll" -reports:coverage.xml -targetdir:.coverage
---
+++ upload-coverage-to-coveralls.cmd
@@ -0,0 +1,11 @@
+csmacnz.coveralls^
+ --opencover^
+ -i coverage.xml^
+ --repoToken %COVERALLS_REPO_TOKEN%^
+ --useRelativePaths^
+ --commitId %APPVEYOR_REPO_COMMIT%^
+ --commitBranch %APPVEYOR_REPO_BRANCH%^
+ --commitAuthor %APPVEYOR_REPO_COMMIT_AUTHOR%^
+ --commitMessage %APPVEYOR_REPO_COMMIT_MESSAGE%^
+ --jobId %APPVEYOR_BUILD_NUMBER%
+ --serviceName appveyor
