3841440aeebc7ebc639d47e785576a036a1ee137
Jorge Candeias <candeias@fastmail.fm>
2019-02-23 23:31:32 +0000

    Updated scripts.

--- create-coverage-report.cmd
+++ create-coverage-report.cmd
@@ -1,4 +1,4 @@
-"%HOMEDRIVE%%HOMEPATH%\.nuget\packages\opencover\4.7.922\tools\OpenCover.Console.exe"^
+@"%HOMEDRIVE%%HOMEPATH%\.nuget\packages\opencover\4.7.922\tools\OpenCover.Console.exe"^
  -target:"%ProgramFiles%\dotnet\dotnet.exe"^
  -targetargs:"test"^
  -output:coverage.xml^
@@ -7,4 +7,4 @@
  -filter:"+[Client]* +[Grains]* +[Grains.Abstractions]* +[Silo]* -[UnitTests]* -[Database]*"^
  -excludebyattribute:"*.GeneratedCode*"
 
-reportgenerator -reports:coverage.xml -targetdir:.coverage
+@reportgenerator -reports:coverage.xml -targetdir:.coverage
--- upload-coverage-to-coveralls.cmd
+++ upload-coverage-to-coveralls.cmd
@@ -1,11 +1,10 @@
-csmacnz.coveralls^
+@csmacnz.coveralls^
  --opencover^
- -i coverage.xml^
+ -i ./coverage.xml^
  --repoToken %COVERALLS_REPO_TOKEN%^
- --useRelativePaths^
  --commitId %APPVEYOR_REPO_COMMIT%^
  --commitBranch %APPVEYOR_REPO_BRANCH%^
  --commitAuthor %APPVEYOR_REPO_COMMIT_AUTHOR%^
+ --commitEmail %APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL%^
  --commitMessage %APPVEYOR_REPO_COMMIT_MESSAGE%^
- --jobId %APPVEYOR_BUILD_NUMBER%^
- --serviceName appveyor
+ --jobId %APPVEYOR_JOB_ID%
