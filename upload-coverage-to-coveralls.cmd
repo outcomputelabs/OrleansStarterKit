00322cd1152c753127fb973ec0a175765b45bf6a
Jorge Candeias <candeias@fastmail.fm>
2019-02-23 23:43:30 +0000

    Updated script.

--- upload-coverage-to-coveralls.cmd
+++ upload-coverage-to-coveralls.cmd
@@ -1,10 +1,7 @@
-@csmacnz.coveralls^
- --opencover^
- -i ./coverage.xml^
- --repoToken %COVERALLS_REPO_TOKEN%^
- --commitId %APPVEYOR_REPO_COMMIT%^
- --commitBranch %APPVEYOR_REPO_BRANCH%^
- --commitAuthor %APPVEYOR_REPO_COMMIT_AUTHOR%^
- --commitEmail %APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL%^
- --commitMessage %APPVEYOR_REPO_COMMIT_MESSAGE%^
- --jobId %APPVEYOR_JOB_ID%
+@csmacnz.coveralls --opencover -i ./coverage.xml --repoToken %COVERALLS_REPO_TOKEN%
+
+ rem --commitId %APPVEYOR_REPO_COMMIT%^
+ rem --commitBranch %APPVEYOR_REPO_BRANCH%^
+ rem --commitAuthor %APPVEYOR_REPO_COMMIT_AUTHOR%^
+ rem --commitMessage %APPVEYOR_REPO_COMMIT_MESSAGE%^
+ rem --jobId %APPVEYOR_JOB_ID%
