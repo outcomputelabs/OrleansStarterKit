7a118c6c52b9d11026f21b8d30ba1ec30f97260b
Jorge Candeias <candeias@fastmail.fm>
2019-03-01 16:48:53 +0000

    Added test.

---
+++ test/UnitTests/ChatMessageTests.cs
@@ -0,0 +1,27 @@
+﻿using Grains.Models;
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ChatMessageTests
+    {
+        [Fact]
+        public void ChatMessage_Holds_Data()
+        {
+            // arrange
+            var id = Guid.NewGuid();
+            var timestamp = DateTime.UtcNow;
+
+            // act
+            var message = new ChatMessage(id, "User1", "User2", "SomeContent", timestamp);
+
+            // assert
+            Assert.Equal(id, message.Id);
+            Assert.Equal("User1", message.FromUserId);
+            Assert.Equal("User2", message.ToUserId);
+            Assert.Equal("SomeContent", message.Content);
+            Assert.Equal(timestamp, message.Timestamp);
+        }
+    }
+}
