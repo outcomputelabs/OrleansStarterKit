fca53b032fe22e3c0824a2adca193fc498465f1b
Jorge Candeias <candeias@fastmail.fm>
2019-03-02 11:38:30 +0000

    Added test.

--- test/UnitTests/ReplaceVersionWithExactValueInPathDocumentFilterTests.cs
+++ test/UnitTests/ReplaceVersionWithExactValueInPathDocumentFilterTests.cs
@@ -30,5 +30,29 @@ namespace UnitTests
             // assert
             Assert.Single(document.Paths.Keys, document.Info.Version);
         }
+
+        [Fact]
+        public void Skips_Filter()
+        {
+            // arrange
+            var filter = new ReplaceVersionWithExactValueInPathDocumentFilter();
+            var document = new SwaggerDocument()
+            {
+                Info = new Info
+                {
+                    Version = "123"
+                },
+                Paths = new Dictionary<string, PathItem>
+                {
+                    { "SomeKey", null }
+                }
+            };
+
+            // act
+            filter.Apply(document, null);
+
+            // assert
+            Assert.Single(document.Paths.Keys, "SomeKey");
+        }
     }
 }
