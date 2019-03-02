6843b4c269605f393862f8570c9c2f470c35462b
Jorge Candeias <candeias@fastmail.fm>
2019-03-02 19:21:30 +0000

    Cleanup.

--- src/Silo/SiloHostedService.cs
+++ src/Silo/SiloHostedService.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using Orleans;
 using Orleans.Configuration;
