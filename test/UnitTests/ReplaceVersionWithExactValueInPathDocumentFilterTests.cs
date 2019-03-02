d83f53db0bcfcf8077909f5c008f896249b4ca98
Jorge Candeias <candeias@fastmail.fm>
2019-03-02 11:34:07 +0000

    Added test.

---
+++ test/UnitTests/ReplaceVersionWithExactValueInPathDocumentFilterTests.cs
@@ -0,0 +1,34 @@
+﻿using Silo;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ReplaceVersionWithExactValueInPathDocumentFilterTests
+    {
+        [Fact]
+        public void Applies_Filter()
+        {
+            // arrange
+            var filter = new ReplaceVersionWithExactValueInPathDocumentFilter();
+            var document = new SwaggerDocument()
+            {
+                Info = new Info
+                {
+                    Version = "123"
+                },
+                Paths = new Dictionary<string, PathItem>
+                {
+                    { "{version}", null }
+                }
+            };
+
+            // act
+            filter.Apply(document, null);
+
+            // assert
+            Assert.Single(document.Paths.Keys, document.Info.Version);
+        }
+    }
+}
