d9ec3f424ff92c1d3efac3ecb0decdfde702f5d3
Jorge Candeias <candeias@fastmail.fm>
2019-03-02 13:54:17 +0000

    Added test.

---
+++ test/UnitTests/Fakes/FakeSiloHostedServiceOptions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Options;
+using Silo.Options;
+
+namespace UnitTests.Fakes
+{
+    public class FakeSiloHostedServiceOptions : IOptions<SiloHostedServiceOptions>
+    {
+        public SiloHostedServiceOptions Value { get; } = new SiloHostedServiceOptions();
+    }
+}
---
+++ test/UnitTests/SiloHostedServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Orleans.Hosting;
+using Silo;
+using System.Reflection;
+using UnitTests.Fakes;
+using Xunit;
+
+namespace UnitTests
+{
+    public class SiloHostedServiceTests
+    {
+        [Fact]
+        public void Has_SiloHost()
+        {
+            // arrange
+            var options = new FakeSiloHostedServiceOptions();
+            options.Value.AdoNetConnectionString = "SomeConnectionString";
+            options.Value.AdoNetInvariant = "SomeInvariant";
+            options.Value.SiloPortRange.Start = 11111;
+            options.Value.ClusterId = "SomeClusterId";
+            options.Value.ServiceId = "SomeServiceId";
+
+            var environment = new FakeHostingEnvironment
+            {
+                EnvironmentName = "SomeEnvironment"
+            };
+
+            // act
+            var service = new SiloHostedService(
+                options,
+                new FakeLoggerProvider(),
+                new FakeNetworkPortFinder(),
+                environment);
+
+            // assert - white box
+            var host = service.GetType().GetField("_host", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(service) as ISiloHost;
+            Assert.NotNull(host);
+        }
+    }
+}
