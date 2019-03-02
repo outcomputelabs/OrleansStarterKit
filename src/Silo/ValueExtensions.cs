315172f42c184982620889c4869f1384cbabdba5
Jorge Candeias <candeias@fastmail.fm>
2019-03-02 11:44:35 +0000

    Added test.

--- src/Silo/ValueExtensions.cs
+++ src/Silo/ValueExtensions.cs
@@ -4,13 +4,9 @@ namespace Silo
 {
     public static class ValueExtensions
     {
-        public static T ValueIf<T>(this T value, T compare, T replace)
+        public static T ValueIf<T>(this T value, T compare, T replace, IEqualityComparer<T> comparer = null)
         {
-            return ValueIf(value, compare, replace, EqualityComparer<T>.Default);
-        }
-
-        public static T ValueIf<T>(this T value, T compare, T replace, IEqualityComparer<T> comparer)
-        {
+            comparer = comparer ?? EqualityComparer<T>.Default;
             return comparer.Equals(value, compare) ? replace : value;
         }
     }
---
+++ test/UnitTests/ValueExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using Silo;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ValueExtensionsTests
+    {
+        [Fact]
+        public void ValueIf_Replaces_Values()
+        {
+            // arrange
+            var value = 1;
+            var compare = 1;
+            var replace = 2;
+
+            // act
+            var actual = value.ValueIf(compare, replace);
+
+            // assert
+            Assert.Equal(replace, actual);
+        }
+    }
+}
