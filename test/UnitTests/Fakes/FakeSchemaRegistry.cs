3c74494d45a07b5fea574fe0a124d29510dfd9d6
Jorge Candeias <candeias@fastmail.fm>
2019-03-02 10:56:54 +0000

    Added test.

---
+++ test/UnitTests/Fakes/FakeSchemaRegistry.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTests.Fakes
+{
+    public class FakeSchemaRegistry : ISchemaRegistry
+    {
+        public IDictionary<string, Schema> Definitions => throw new NotSupportedException();
+
+        public Schema GetOrRegister(Type type)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ test/UnitTests/RemoveVersionFromParameterOperationFilterTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Moq;
+using Silo;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using UnitTests.Fakes;
+using Xunit;
+
+namespace UnitTests
+{
+    public class RemoveVersionFromParameterOperationFilterTests
+    {
+        [Fact]
+        public void Applies_Filter()
+        {
+            // arrange
+            var filter = new RemoveVersionFromParameterOperationFilter();
+            var parameter = new Mock<IParameter>();
+            parameter.Setup(_ => _.Name).Returns("version");
+            var operation = new Operation
+            {
+                Parameters = new List<IParameter>
+                {
+                    parameter.Object
+                }
+            };
+            var context = new OperationFilterContext(new ApiDescription(), new FakeSchemaRegistry(), new FakeMethodInfo());
+
+            // act
+            filter.Apply(operation, context);
+
+            // assert
+            Assert.Empty(operation.Parameters);
+        }
+    }
+}
