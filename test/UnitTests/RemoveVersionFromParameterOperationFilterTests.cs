c80d3034ca15526a06007a2764015984d560ca95
Jorge Candeias <candeias@fastmail.fm>
2019-03-02 11:25:10 +0000

    Added test.

--- test/UnitTests/RemoveVersionFromParameterOperationFilterTests.cs
+++ test/UnitTests/RemoveVersionFromParameterOperationFilterTests.cs
@@ -33,5 +33,28 @@ namespace UnitTests
             // assert
             Assert.Empty(operation.Parameters);
         }
+
+        [Fact]
+        public void Skips_Filter()
+        {
+            // arrange
+            var filter = new RemoveVersionFromParameterOperationFilter();
+            var parameter = new Mock<IParameter>();
+            parameter.Setup(_ => _.Name).Returns("other");
+            var operation = new Operation
+            {
+                Parameters = new List<IParameter>
+                {
+                    parameter.Object
+                }
+            };
+            var context = new OperationFilterContext(new ApiDescription(), new FakeSchemaRegistry(), new FakeMethodInfo());
+
+            // act
+            filter.Apply(operation, context);
+
+            // assert
+            Assert.Single(operation.Parameters, parameter.Object);
+        }
     }
 }
