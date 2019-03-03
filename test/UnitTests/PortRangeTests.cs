8f878a911e35ac0d0e62b3eebfb87ea1b1eb94ad
Jorge Candeias <candeias@fastmail.fm>
2019-03-03 12:16:22 +0000

    Added test.

---
+++ test/UnitTests/PortRangeTests.cs
@@ -0,0 +1,27 @@
+﻿using Silo;
+using Xunit;
+
+namespace UnitTests
+{
+    public class PortRangeTests
+    {
+        [Fact]
+        public void Holds_Data()
+        {
+            // arrange
+            var start = 11111;
+            var end = 22222;
+
+            // act
+            var range = new PortRange
+            {
+                Start = start,
+                End = end
+            };
+
+            // assert
+            Assert.Equal(start, range.Start);
+            Assert.Equal(end, range.End);
+        }
+    }
+}
