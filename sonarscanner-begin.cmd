37630b429b32156c9de3db5455931a065440de2c
Jorge Candeias <candeias@fastmail.fm>
2019-03-04 11:47:50 +0000

    Added coverlet output to sonar.

--- sonarscanner-begin.cmd
+++ sonarscanner-begin.cmd
@@ -1,1 +1,1 @@
-@dotnet sonarscanner begin /k:"%SONARQUBE_PROJECT_KEY%" /o:"%SONARQUBE_ORGANIZATION%" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%SONARQUBE_TOKEN%" /d:sonar.cs.opencover.reportsPaths="coverage.xml" /d:sonar.exclusions="src/Database/**"
+@dotnet sonarscanner begin /k:"%SONARQUBE_PROJECT_KEY%" /o:"%SONARQUBE_ORGANIZATION%" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%SONARQUBE_TOKEN%" /d:sonar.cs.opencover.reportsPaths="**/coverage.opencover.xml" /d:sonar.exclusions="src/Database/**/*"
