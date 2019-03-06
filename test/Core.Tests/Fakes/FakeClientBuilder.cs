7c27edf5cf0cfa51f7d025306865b54cd1c84dbd
Jorge Candeias <candeias@fastmail.fm>
2019-03-06 17:19:58 +0000

    Added test.

---
+++ test/Core.Tests/Fakes/FakeClientBuilder.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans;
+using Orleans.Hosting;
+using System;
+using System.Collections.Generic;
+
+namespace Core.Tests.Fakes
+{
+    public class FakeClientBuilder : IClientBuilder
+    {
+        public IDictionary<object, object> Properties => throw new NotImplementedException();
+
+        public IClusterClient Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IClientBuilder ConfigureAppConfiguration(Action<HostBuilderContext, IConfigurationBuilder> configureDelegate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IClientBuilder ConfigureContainer<TContainerBuilder>(Action<TContainerBuilder> configureContainer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IClientBuilder ConfigureHostConfiguration(Action<IConfigurationBuilder> configureDelegate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IClientBuilder ConfigureServices(Action<HostBuilderContext, IServiceCollection> configureDelegate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IClientBuilder UseServiceProviderFactory<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
