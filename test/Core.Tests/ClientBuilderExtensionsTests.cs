60c7ce6522624e88869b66c683f8ca605f164069
Jorge Candeias <candeias@fastmail.fm>
2019-03-06 17:16:25 +0000

    Added test.

---
+++ test/Core.Tests/ClientBuilderExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Orleans;
+using System;
+using Xunit;
+
+namespace Core.Tests
+{
+    public class ClientBuilderExtensionsTests
+    {
+        [Fact]
+        public void TryUseLocalhostClustering_RefusesNullBuilder()
+        {
+            IClientBuilder builder = null;
+
+            var error = Assert.Throws<ArgumentNullException>(() =>
+            {
+                builder.TryUseLocalhostClustering(new ConfigurationBuilder().Build());
+            });
+            Assert.Equal("builder", error.ParamName);
+        }
+    }
+}
