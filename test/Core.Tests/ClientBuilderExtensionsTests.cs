aa0420b524198582a6a2587ee0abd1a5ff96c48b
Jorge Candeias <candeias@fastmail.fm>
2019-03-06 21:32:31 +0000

    Added test.

--- test/Core.Tests/ClientBuilderExtensionsTests.cs
+++ test/Core.Tests/ClientBuilderExtensionsTests.cs
@@ -7,6 +7,7 @@ using Orleans.Configuration;
 using System;
 using System.Collections.Generic;
 using Xunit;
+using Moq;
 
 namespace Core.Tests
 {
@@ -27,11 +28,11 @@ namespace Core.Tests
         [Fact]
         public void TryUseLocalhostClustering_RefusesNullConfiguration()
         {
-            var builder = new FakeClientBuilder();
+            var builder = new Mock<IClientBuilder>();
 
             var error = Assert.Throws<ArgumentNullException>(() =>
             {
-                builder.TryUseLocalhostClustering(null);
+                builder.Object.TryUseLocalhostClustering(null);
             });
             Assert.Equal("configuration", error.ParamName);
         }
@@ -82,13 +83,71 @@ namespace Core.Tests
         [Fact]
         public void TryUseAdoNetClustering_RefusesNullConfiguration()
         {
-            var builder = new FakeClientBuilder();
+            var builder = new Mock<IClientBuilder>();
 
             var error = Assert.Throws<ArgumentNullException>(() =>
             {
-                builder.TryUseAdoNetClustering(null);
+                builder.Object.TryUseAdoNetClustering(null);
             });
             Assert.Equal("configuration", error.ParamName);
+        }
+
+        [Fact]
+        public void TryUseAdoNetClustering_AppliesConfiguration()
+        {
+            // arrange
+            var builder = new ClientBuilder();
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "Orleans:Providers:Clustering:Provider", "AdoNet" },
+                    { "Orleans:Providers:Clustering:AdoNet:ConnectionStringName", "SomeConnectionStringName" },
+                    { "Orleans:Providers:Clustering:AdoNet:Invariant", "SomeInvariant" },
+                    { "Orleans:ServiceId", "SomeServiceId" },
+                    { "Orleans:ClusterId", "SomeClusterId" },
+                    { "ConnectionStrings:SomeConnectionStringName", "SomeConnectionString" }
+                })
+                .Build();
+
+            // act
+            builder.TryUseAdoNetClustering(config);
+
+            // assert the cluster options are there
+            var client = builder.Build();
+            var clusterOptions = client.ServiceProvider.GetService<IOptions<ClusterOptions>>();
+            Assert.NotNull(clusterOptions);
+            Assert.Equal("SomeClusterId", clusterOptions.Value.ClusterId);
+            Assert.Equal("SomeServiceId", clusterOptions.Value.ServiceId);
+
+            // assert the adonet client options are there
+            var adonetOptions = client.ServiceProvider.GetService<IOptions<AdoNetClusteringClientOptions>>();
+            Assert.NotNull(adonetOptions);
+            Assert.Equal("SomeConnectionString", adonetOptions.Value.ConnectionString);
+            Assert.Equal("SomeInvariant", adonetOptions.Value.Invariant);
+        }
+
+        [Fact]
+        public void TryUseAdoNetClustering_IgnoresConfiguration()
+        {
+            // arrange
+            var builder = new Mock<IClientBuilder>();
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "Orleans:Providers:Clustering:Provider", "None" },
+                    { "Orleans:Providers:Clustering:AdoNet:ConnectionStringName", "SomeConnectionStringName" },
+                    { "Orleans:Providers:Clustering:AdoNet:Invariant", "SomeInvariant" },
+                    { "Orleans:ServiceId", "SomeServiceId" },
+                    { "Orleans:ClusterId", "SomeClusterId" },
+                    { "ConnectionStrings:SomeConnectionStringName", "SomeConnectionString" }
+                })
+                .Build();
+
+            // act
+            builder.Object.TryUseAdoNetClustering(config);
+
+            // assert that nothing happened
+            builder.VerifyNoOtherCalls();
         }
     }
 }
