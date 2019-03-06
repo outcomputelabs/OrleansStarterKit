81c6253c778ccf49049446067f8cfc7eeb6dbb17
Jorge Candeias <candeias@fastmail.fm>
2019-03-06 16:47:15 +0000

    Added test.

---
+++ test/Client.Console.Tests/ConsoleClientHostedServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace Client.Console.Tests
+{
+    public class ConsoleClientHostedServiceTests
+    {
+        [Fact]
+        public void RefusesNullClusterClient()
+        {
+            var error = Assert.Throws<ArgumentNullException>(() =>
+            {
+                new ConsoleClientHostedService(null);
+            });
+            Assert.Equal("client", error.ParamName);
+        }
+    }
+}
