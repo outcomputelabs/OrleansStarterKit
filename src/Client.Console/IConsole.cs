d59e22e98eb5384f331ffc490785b24914507cdf
Jorge Candeias <candeias@fastmail.fm>
2019-03-07 19:07:58 +0000

    WIP

---
+++ src/Client.Console/IConsole.cs
@@ -0,0 +1,10 @@
+﻿namespace Client.Console
+{
+    /// <summary>
+    /// Abstracts the console for easier testing.
+    /// </summary>
+    public interface IConsole
+    {
+        void WriteLine(string format, params object[] args);
+    }
+}
