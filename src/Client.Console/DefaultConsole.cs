7b6421edbdc7e0f5bfa61bce92639427a75da824
Jorge Candeias <candeias@fastmail.fm>
2019-03-07 19:07:58 +0000

    WIP

---
+++ src/Client.Console/DefaultConsole.cs
@@ -0,0 +1,14 @@
+﻿namespace Client.Console
+{
+    /// <summary>
+    /// Default implementation of <see cref="IConsole"/>.
+    /// Redirects everything to the <see cref="System.Console"/>.
+    /// </summary>
+    public class DefaultConsole : IConsole
+    {
+        public void WriteLine(string format, params object[] args)
+        {
+            System.Console.WriteLine(format, args);
+        }
+    }
+}
---
+++ test/Client.Console.Tests/DefaultConsoleTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Xunit;
+
+namespace Client.Console.Tests
+{
+    public class DefaultConsoleTests
+    {
+        [Fact]
+        public void WriteLine_WritesToSystemConsole()
+        {
+            var console = new DefaultConsole();
+            using (var output = new MemoryStream())
+            using (var writer = new StreamWriter(output))
+            using (var reader = new StreamReader(output))
+            {
+                System.Console.SetOut(writer);
+
+                console.WriteLine("SomeFormatString {0}", 123);
+                writer.Flush();
+
+                output.Position = 0;
+
+                Assert.Equal("SomeFormatString 123", reader.ReadLine());
+            }
+        }
+    }
+}
