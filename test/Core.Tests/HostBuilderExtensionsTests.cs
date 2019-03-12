5123801ad19b30f672bbf7343debf8fb7bccdb2a
Jorge Candeias <candeias@fastmail.fm>
2019-03-12 22:54:56 +0000

    Added test.

---
+++ test/Core.Tests/HostBuilderExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using Xunit;
+
+namespace Core.Tests
+{
+    public class HostBuilderExtensionsTests
+    {
+        [Fact]
+        public void UseSharedConfiguration_RefusesNullBuilder()
+        {
+            IHostBuilder builder = null;
+
+            var error = Assert.Throws<ArgumentNullException>(() =>
+            {
+                builder.UseSharedConfiguration();
+            });
+            Assert.Equal("builder", error.ParamName);
+        }
+    }
+}
