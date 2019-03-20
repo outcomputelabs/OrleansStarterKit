5d5ff958eeadca9a9d04b3db11fb74920012e0b2
Jorge Candeias <candeias@fastmail.fm>
2019-03-20 16:53:56 +0000

    Added AddOrUpdateAsync

--- src/Grains.Interfaces/IChatRoomListGrain.cs
+++ src/Grains.Interfaces/IChatRoomListGrain.cs
@@ -1,8 +1,11 @@
-﻿using Orleans;
+﻿using Grains.Models;
+using Orleans;
+using System.Threading.Tasks;
 
 namespace Grains
 {
     public interface IChatRoomListGrain : IGrainWithGuidKey
     {
+        Task AddOrUpdateAsync(ChatRoomInfo info);
     }
 }
--- src/Grains/ChatRoomListGrain.cs
+++ src/Grains/ChatRoomListGrain.cs
@@ -7,6 +7,14 @@ namespace Grains
 {
     public class ChatRoomListGrain : Grain<ChatRoomListState>, IChatRoomListGrain
     {
+        public Task AddOrUpdateAsync(ChatRoomInfo info)
+        {
+            State.List.Remove(info);
+            State.List.Add(info);
+
+            return WriteStateAsync();
+        }
+
         public override Task OnActivateAsync()
         {
             if (State.List == null) State.List = new HashSet<ChatRoomInfo>();
