5971d5c492c7c74a684a91cc6451ad2e1f066b2b
Jorge Candeias <candeias@fastmail.fm>
2019-03-22 00:04:28 +0000

    Added grains.

---
+++ src/Grains.Interfaces/IUserRegistryGrain.cs
@@ -0,0 +1,15 @@
+﻿using Grains.Models;
+using Orleans;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public interface IUserRegistryGrain : IGrainWithGuidKey
+    {
+        /// <summary>
+        /// Attempts to create a new user with the given key and username.
+        /// The username check is case-insensitive.
+        /// </summary>
+        Task<bool> TryCreateAsync(UserInfo info);
+    }
+}
