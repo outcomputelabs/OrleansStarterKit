907f74df497faf92dfd79fa37143fd3e24d05a4c
Jorge Candeias <candeias@fastmail.fm>
2019-03-22 18:11:26 +0000

    Added SetInfoAsync()

--- src/Grains.Interfaces/IUserGrain.cs
+++ src/Grains.Interfaces/IUserGrain.cs
@@ -1,8 +1,11 @@
-﻿using Orleans;
+﻿using Grains.Models;
+using Orleans;
+using System.Threading.Tasks;
 
 namespace Grains
 {
     public interface IUserGrain : IGrainWithGuidKey
     {
+        Task SetInfoAsync(UserInfo info);
     }
 }
