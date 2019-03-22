0f71f159c5b26248654970da20793f032350c0df
Jorge Candeias <candeias@fastmail.fm>
2019-03-22 18:11:26 +0000

    Added SetInfoAsync()

--- src/Grains/UserGrain.cs
+++ src/Grains/UserGrain.cs
@@ -1,8 +1,25 @@
-﻿using Orleans;
+﻿using Grains.Models;
+using Orleans;
+using System;
+using System.Threading.Tasks;
 
 namespace Grains
 {
-    public class UserGrain : Grain, IUserGrain
+    public class UserGrain : Grain<UserState>, IUserGrain
     {
+        public Task SetInfoAsync(UserInfo info)
+        {
+            if (info.Key != GrainKey) throw new InvalidOperationException();
+
+            State.Info = info;
+            return WriteStateAsync();
+        }
+
+        private Guid GrainKey => this.GetPrimaryKey();
+    }
+
+    public class UserState
+    {
+        public UserInfo Info { get; set; }
     }
 }
