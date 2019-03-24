5ac8d26166dbdee180de7f285ea5a72e67508caf
Jorge Candeias <candeias@fastmail.fm>
2019-03-24 23:58:02 +0000

    Redesign.

---
+++ src/Grains.Interfaces/IPlayerGrain.cs
@@ -0,0 +1,12 @@
+﻿using Grains.Models;
+using Orleans;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public interface IPlayerGrain : IGrainWithStringKey
+    {
+        Task SetInfoAsync(PlayerInfo info);
+        Task<PlayerInfo> GetInfoAsync();
+    }
+}
