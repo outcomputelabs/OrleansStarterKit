c5fca4063b873d219471d20fda5d442f11e52839
Jorge Candeias <candeias@fastmail.fm>
2019-03-26 23:28:40 +0000

    Added PersistenceServiceClient

---
+++ src/Grains/PersistenceServiceClient.cs
@@ -0,0 +1,12 @@
+﻿using Orleans.Runtime.Services;
+using System;
+
+namespace Grains
+{
+    public class PersistenceServiceClient : GrainServiceClient<IPersistenceService>, IPersistenceServiceClient
+    {
+        public PersistenceServiceClient(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
