7ceb433741f06c79ab59c7983c55cc60f7c76e24
Jorge Candeias <candeias@fastmail.fm>
2019-03-30 20:26:14 +0000

    Added the user registry grain.

--- src/Grains.Interfaces/IUserRegistryGrain.cs
+++ src/Grains.Interfaces/IUserRegistryGrain.cs
@@ -1,5 +1,6 @@
 ﻿using Grains.Models;
 using Orleans;
+using System;
 using System.Threading.Tasks;
 
 namespace Grains
@@ -11,11 +12,16 @@ namespace Grains
         /// If the entity does not yet exist, it will be added.
         /// If the entity already exists, it will be updated.
         /// </summary>
-        Task RegisterAsync(UserInfo info);
+        Task RegisterAsync(UserInfo entity);
 
         /// <summary>
         /// Unregisters an entity from the registry.
         /// </summary>
-        Task UnregisterAsync(UserInfo info);
+        Task UnregisterAsync(UserInfo entity);
+
+        /// <summary>
+        /// Returns the entity with the given identifier or null if no corresponding entity exists.
+        /// </summary>
+        Task<UserInfo> GetAsync(Guid id);
     }
 }
