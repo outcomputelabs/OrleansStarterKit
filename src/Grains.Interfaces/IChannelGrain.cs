18390d83ef88d61e2cf215f937949c807d0aba0b
Jorge Candeias <candeias@fastmail.fm>
2019-03-31 16:43:38 +0100

    Added channel grain.

--- src/Grains.Interfaces/IChannelGrain.cs
+++ src/Grains.Interfaces/IChannelGrain.cs
@@ -1,8 +1,15 @@
-﻿using Orleans;
+﻿using Grains.Models;
+using Orleans;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
 
 namespace Grains
 {
     public interface IChannelGrain : IGrainWithGuidKey
     {
+        Task SetInfoAsync(ChannelInfo info);
+        Task<ChannelInfo> GetInfoAsync();
+        Task TellAsync(Message message);
+        Task<ImmutableList<Message>> GetLatestMessagesAsync();
     }
 }
---
+++ src/Grains/Options/ChannelOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Grains.Options
+{
+    public class ChannelOptions
+    {
+        public int MaxCachedMessages { get; set; }
+    }
+}
