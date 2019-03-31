55068c196555111be36db0933d5512021a2b14ca
Jorge Candeias <candeias@fastmail.fm>
2019-03-31 15:18:39 +0100

    Added ChannelInfo

---
+++ src/Grains.Interfaces/Models/ChannelInfo.cs
@@ -0,0 +1,20 @@
+﻿using Orleans.Concurrency;
+using System;
+
+namespace Grains.Models
+{
+    [Immutable]
+    public class ChannelInfo
+    {
+        public ChannelInfo(Guid id, string handle, string description)
+        {
+            Id = id;
+            Handle = handle;
+            Description = description;
+        }
+
+        public Guid Id { get; }
+        public string Handle { get; }
+        public string Description { get; }
+    }
+}
