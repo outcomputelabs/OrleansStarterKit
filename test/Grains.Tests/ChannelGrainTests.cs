5010d7b70d621038a8e5d3200ef9e5e68b075f7e
Jorge Candeias <candeias@fastmail.fm>
2019-03-31 22:25:02 +0100

    Added test

---
+++ test/Grains.Tests/ChannelGrainTests.cs
@@ -0,0 +1,44 @@
+﻿using Grains.Models;
+using Orleans.TestingHost;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Grains.Tests
+{
+    [Collection(nameof(ClusterCollection))]
+    public class ChannelGrainTests
+    {
+        private readonly ClusterFixture _fixture;
+
+        public ChannelGrainTests(ClusterFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task SetInfoAsync_Saves_State()
+        {
+            // arrange
+            var id = Guid.NewGuid();
+            var handle = "SomeHandle";
+            var description = "SomeDescription";
+            var info = new ChannelInfo(id, handle, description);
+
+            // act
+            await _fixture.Cluster.GrainFactory
+                .GetGrain<IChannelGrain>(id)
+                .SetInfoAsync(info);
+
+            // assert the state was saved
+            var state = await _fixture.SiloServiceProvider.GetService<RegistryContext>()
+                .Channels
+                .FindAsync(id);
+
+            Assert.Equal(info.Id, state.Id);
+            Assert.Equal(info.Handle, state.Handle);
+            Assert.Equal(info.Description, state.Description);
+        }
+    }
+}
