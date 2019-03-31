316634110d4d2e498cdc15bf832a77d5eaf21d50
Jorge Candeias <candeias@fastmail.fm>
2019-03-31 15:54:16 +0100

    Added channels table.

---
+++ src/Database/Tables/Channels.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[Channels]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[Handle] NVARCHAR(100) NOT NULL,
+	[Description] NVARCHAR(1000) NOT NULL
+)
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [UK_Channels_Id]
+ON [dbo].[Channels]
+(
+	[Id]
+)
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX [UK_Channels_Handle]
+ON [dbo].[Channels]
+(
+	[Handle]
+)
+GO
